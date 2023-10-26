dd4ef9ff72eea30582fb96aa9e43cd5fffca249b
EzekielCz <ezechielcazacu@gmail.com>
2023-10-26 18:11:02 -0500

    command class

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,13 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("Command type required.", function (){
+    let output;
+    expect(output=new Command ("Array").commandType).toBe("Array");
+  });
+  it("constructor sets a value passed in as the 2nd argument", function(){
+    let output;
+    expect(output = new Command("Array","String").value).toBe("String");
+  });
 
 });
