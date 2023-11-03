3b584cbcc328eff9115cf50d08d5c3ca42f43533
EzekielCz <ezechielcazacu@gmail.com>
2023-11-02 19:50:58 -0500

    Mars Rover

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -9,12 +9,16 @@ describe("Command class", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
   it("Command type required.", function (){
-    let output;
-    expect(output=new Command ("Array").commandType).toBe("Array");
+    // let output;
+    // expect(output=new Command ("Array").commandType
+    let output = new Command ("Array");
+    expect(output.commandType).toBe("Array");
   });
   it("constructor sets a value passed in as the 2nd argument", function(){
-    let output;
-    expect(output = new Command("Array","String").value).toBe("String");
+    let output = new Command ("Array","String");
+    expect(output.value).toBe("String");
+    // let output;
+    // expect(output = new Command("Array","String").value).toBe("String");
   });
 
 });
